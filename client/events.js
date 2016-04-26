2d33da42dec6d8173411ce95eaebd9134215e647
Teody C. Seguin <teodyseguin@gmail.com>
2016-04-26 20:41:36 +0800

    attached the event for filter

--- client/events.js
+++ client/events.js
@@ -2,11 +2,13 @@
   $(document).ready(function() {
     Map.initMapping();
 
-    $('#save-car').click(App.save);
     $('#cancel-car').click(App.cancel);
     $('#car-name').change(App.change);
     $('#car-color').change(App.change);
     $('#car-weight').change(App.change);
+    $('#filter-color').change(function() {
+      App.filter($(this));
+    });
   });
 
   // To keep the table row bindings, whenever the table is reloaded
@@ -17,5 +19,8 @@
   $(document).on('click', '#cars-table .delete', {}, function(e) {
     e.preventDefault();
     App.remove($(this));
+  });
+  $(document).on('click', '#save-car', {}, function(e) {
+    App.save();
   });
 })(jQuery, App, Map);
