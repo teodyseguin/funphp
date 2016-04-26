18c6fc3fca58e45501a6ca7c284f3d2a74d584c8
Teody C. Seguin <teodyseguin@gmail.com>
2016-04-26 20:40:47 +0800

    Updated cars.sql
    * contains few data for demonstration
    * change weight column from int to varchar

--- cars.sql
+++ cars.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2016 at 05:29 AM
+-- Generation Time: Apr 26, 2016 at 12:38 PM
 -- Server version: 5.6.25
 -- PHP Version: 5.5.29
 
@@ -30,9 +30,25 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `id` int(20) unsigned NOT NULL,
   `name` varchar(100) NOT NULL,
   `color` varchar(15) DEFAULT NULL,
-  `weight` int(11) NOT NULL,
+  `weight` varchar(20) NOT NULL,
   `date_time` bigint(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`id`, `name`, `color`, `weight`, `date_time`) VALUES
+(1, 'Car A', 'blue', '01', 1461669767386),
+(2, 'Car B', 'blue', '01', 1461674245785),
+(3, 'Car C', 'red', '03', 1461669938044),
+(4, 'Car D', 'green', '01', 1461674175412),
+(5, 'Car E', 'red', '05', 1461670029097),
+(6, 'Car F', 'blue', '06', 1461670044562),
+(7, 'Car G', 'red', '01', 1461674012543),
+(8, 'Car H', 'blue', '01', 1461674263535),
+(9, 'Car I', 'red', '01', 1461670168972),
+(10, 'Car J', 'blue', '01', 1461670148637);
 
 --
 -- Indexes for dumped tables
@@ -53,7 +69,7 @@ ALTER TABLE `cars`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `id` int(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
