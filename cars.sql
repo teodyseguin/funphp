1c4bbf88220204fb0cc419817b892c72ad9a33a4
Teody C. Seguin <teodyseguin@gmail.com>
2016-05-04 13:09:38 +0800

    SQL update
    * Added sub weight structure to sql
    * Include some test data

--- cars.sql
+++ cars.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2016 at 12:38 PM
+-- Generation Time: May 04, 2016 at 05:01 AM
 -- Server version: 5.6.25
 -- PHP Version: 5.5.29
 
@@ -31,24 +31,24 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `name` varchar(100) NOT NULL,
   `color` varchar(15) DEFAULT NULL,
   `weight` varchar(20) NOT NULL,
+  `sub_weight` varchar(20) DEFAULT NULL,
   `date_time` bigint(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`id`, `name`, `color`, `weight`, `date_time`) VALUES
-(1, 'Car A', 'blue', '01', 1461669767386),
-(2, 'Car B', 'blue', '01', 1461674245785),
-(3, 'Car C', 'red', '03', 1461669938044),
-(4, 'Car D', 'green', '01', 1461674175412),
-(5, 'Car E', 'red', '05', 1461670029097),
-(6, 'Car F', 'blue', '06', 1461670044562),
-(7, 'Car G', 'red', '01', 1461674012543),
-(8, 'Car H', 'blue', '01', 1461674263535),
-(9, 'Car I', 'red', '01', 1461670168972),
-(10, 'Car J', 'blue', '01', 1461670148637);
+INSERT INTO `cars` (`id`, `name`, `color`, `weight`, `sub_weight`, `date_time`) VALUES
+(49, 'car a', 'blue', '01', '1', 1462337255243),
+(50, 'car b', 'blue', '02', '2', 1462337260955),
+(52, 'car d', 'blue', '04', '4', 1462337281483),
+(57, 'car i', 'blue', '09', '9', 1462337322321),
+(58, 'car j', 'blue', '10', '10', 1462337334976),
+(59, 'car k', 'blue', '11', '10', 1462337410870),
+(60, 'car l', 'blue', '12', '12', 1462337677501),
+(61, 'car m', 'blue', '13', '13', 1462337761682),
+(62, 'car n', 'red', '14', '14', 1462338048186);
 
 --
 -- Indexes for dumped tables
@@ -69,7 +69,7 @@ ALTER TABLE `cars`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `id` int(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=63;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
